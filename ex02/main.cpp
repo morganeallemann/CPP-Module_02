bb588c2e83bfde83c02f313b78aef1a15ed1bc7a
malleman <malleman@student.42lausanne.ch>
2024-01-22 16:25:55 +0100

    finished

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -22,6 +22,6 @@ std::cout << a << std::endl;
 std::cout << a++ << std::endl;
 std::cout << a << std::endl;
 std::cout << b << std::endl;
-std::cout << Fixed::min( a, b ) << std::endl;
+std::cout << Fixed::max( a, b ) << std::endl;
 return 0;
 }
